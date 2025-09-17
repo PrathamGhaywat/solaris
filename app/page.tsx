84363cbd6345f346700c0ff81cb7dfd64104a84c
atomisadev <atomdevelops@pm.me>
2025-09-17 19:27:09 -0400

    moved the text 5vw to the left

--- app/page.tsx
+++ app/page.tsx
@@ -20,21 +20,22 @@ export default function Home() {
           initial={{ borderColor: "rgba(0, 0, 0, 0)" }}
           animate={{ borderColor: "#FFFFFF2a" }}
           transition={{ duration: 2 }}
-          className="flex justify-center md:flex-row flex-col mt-40 w-[90vw] md:h-[50vh] md:pr-4 gap-6 border-b md:border-r border-[#FFFFFF2a] md:w-[85vw] ml-[2rem] md:ml-[10rem]"
+          className="flex justify-center md:flex-row flex-col mt-40 w-[90vw] md:h-[50vh] md:pr-4 gap-6 border-b md:border-r border-[#FFFFFF2a] md:w-[85vw] ml-[5vw] md:ml-[10rem]"
         >
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ duration: 1 }}
-            className="tracking-tighter	 w-full md:w-2/3 md:pt-6 pt-20 md:p-0 font-bold	text-[4.2rem] md:text-[6rem] 2xl:text-[8rem] leading-none md:pb-12"
+            className="tracking-tighter w-full md:w-2/3 md:pt-6 pt-20 md:p-0 font-bold	text-[4.2rem] md:text-[6rem] 2xl:text-[8rem] leading-none md:pb-12"
           >
             <h1>Build your next project to the moon</h1>
           </motion.div>
           <motion.div
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
-            transition={{ duration: 2, delay:1 }}
-            className="w-4/5 md:w-1/3 md:p-0 pb-12 font-light text-sm 2xl:text-lg text-[#AAA]">
+            transition={{ duration: 2, delay: 1 }}
+            className="w-4/5 md:w-1/3 md:p-0 pb-12 font-light text-sm 2xl:text-lg text-[white]"
+          >
             <h1>
               The year is 2035. Settlers at Tranquility Base on the Moon,
               established in 2033, grapple with life-threatening problems.
@@ -47,8 +48,8 @@ export default function Home() {
         <motion.div
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
-          transition={{ duration: 2, delay:0.5 }}
-          className="md:w-[10vw] w-[20vw] ml-[2rem] md:ml-[10rem] h-[4px] bg-[#FFF]"
+          transition={{ duration: 2, delay: 0.5 }}
+          className="md:w-[10vw] w-[20vw] ml-[5vw] md:ml-[10rem] h-[4px] bg-[#FFF]"
         ></motion.div>
       </div>
     </>
