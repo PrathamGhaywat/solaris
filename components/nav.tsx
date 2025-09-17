f78f123d15ca7e223c1c0483dba5bd5f9a4640b9
atomisadev <atomdevelops@pm.me>
2025-09-17 19:28:10 -0400

    aligned solaris logo with left side

--- components/nav.tsx
+++ components/nav.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Nav() {
   return (
-    <div className="fixed px-12 py-6 z-99">
+    <div className="fixed px-[5vw] md:px-[10rem] py-6 z-99">
       <Image src="/logo.png" alt="logo" width={200} height={0} />
     </div>
   );
